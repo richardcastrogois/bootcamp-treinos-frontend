d7945af121149aeaf9460bac6c798813d099e0de
richardcastrogois <richard.castrogois@gmail.com>
2026-03-12 10:22:50 -0300

    "fix: allow ufs image domain"

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "*.ufs.sh",
+        pathname: "/**",
       },
     ],
   },
